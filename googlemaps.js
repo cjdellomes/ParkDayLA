9f01e71f72a14c6306239e261e590d12037571c5
Chris Dellomes <cjdellomes@gmail.com>
2015-11-07 15:46:00 -0800

    Geocoding works

--- googlemaps.js
+++ googlemaps.js
@@ -4,29 +4,31 @@ var infowindow;
 
 function initialize() {
   var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+  var geocoder = new google.maps.Geocoder();
 
-  map = new google.maps.Map(document.getElementById("googleMap"), {
+  map = new google.maps.Map(document.getElementById('googleMap'), {
       center: pyrmont,
       zoom: 15
     });
 
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    types: ['store']
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
+  document.getElementById('park-button').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
 }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
     }
-  }
+  });
 }
 
 initialize();
