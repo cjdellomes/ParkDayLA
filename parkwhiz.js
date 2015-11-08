b95d76a193ee7c52bf822846894d59722c1f27d7
Peyton Cross <pcross2@lion.lmu.edu>
2015-11-07 18:58:52 -0800

    Finally got Park Meter to work

--- parkwhiz.js
+++ parkwhiz.js
@@ -8,8 +8,33 @@ $(function () {
 			).done(function(result){
 				console.log(result);
 				var searchResult = $("#address").val().toUpperCase();
-				outputResult = []
+				var untouched = searchResult.slice(0,searchResult.indexOf(","));
 				searchResult = searchResult.slice(0,searchResult.indexOf(","));
+				addressPrepInt= searchResult.slice(0,5);
+				console.log("addressPrepInt is "+addressPrepInt);
+				var addressNumber = parseInt(searchResult);
+				console.log("The addressNumber is "+addressNumber);
+				var modAddressNumber = addressNumber%100;
+				console.log("1600 % 100 is " + (1600%100));
+				console.log("modAddressNumber is "+modAddressNumber);
+				if(modAddressNumber < 50) {
+					addressNumber = addressNumber - modAddressNumber;
+					console.log("addressNumber is " + addressNumber)
+				}
+				else {
+					addressNumber += 100 - modAddressNumber;
+					console.log("addressNumber is " + addressNumber)
+					console.log("Round up is being called")
+				}
+
+				console.log(addressNumber);
+
+				var searchResult = String(addressNumber) + untouched.slice(searchResult.indexOf(" "));
+				console.log(searchResult);
+				
+				
+				outputResult = []
+				
 				var isMetered;
 				for (var i = 0; i < result.length; i++) {
 					if(result[i].street_address.includes(searchResult)) {
