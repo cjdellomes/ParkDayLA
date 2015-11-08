8c6144a273c816a47fb671f0bf390fcb803a6280
Chris Dellomes <cjdellomes@gmail.com>
2015-11-07 16:09:01 -0800

    Adjusted location search

--- googlemaps.js
+++ googlemaps.js
@@ -3,7 +3,7 @@ var service;
 var infowindow;
 
 function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+  var pyrmont = new google.maps.LatLng(34.024212,-118.496475);
   var geocoder = new google.maps.Geocoder();
 
   map = new google.maps.Map(document.getElementById('googleMap'), {
@@ -21,10 +21,6 @@ function geocodeAddress(geocoder, resultsMap) {
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
